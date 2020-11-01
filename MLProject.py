adab0b6ca6fefac4a235a21be091a227ae4b64a7
unknown <nkaiser@mymail.mines.edu>
2020-10-31 20:09:00 -0600

    added file reader function

---
+++ MLProject.py
@@ -0,0 +1,9 @@
+def readInputFile(fileName):
+    outputList = [];
+    file = open(fileName, "r")
+    for line in file:
+        outputList.append(line[:len(line)-1])#the -1 gets rid of newline characters
+    return outputList
+
+
+
